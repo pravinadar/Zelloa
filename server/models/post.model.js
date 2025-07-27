5b6dd2108c63418ffaa919169a8d2eeab4711d2b
pravinadar <pnad764@gmail.com>
2025-07-27 17:02:47 +0530

    Added Mongoose models for Post, Comment, and User

---
+++ server/models/post.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    text: {
+        type: String,
+        trim: true,
+    },
+    media: {
+        type: String,
+        default: "",
+        trim: true,
+    },
+    public_id: {
+        type: String,
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }]
+
+}, { timestamps: true });
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
---
+++ server/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false,
+    },
+    bio:{
+        type: String,
+        default: "",
+        trim: true,
+    },
+    profilePicture:{
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        trim: true,
+    },
+    public_id:{
+        type: String,
+        trim: true,
+    },
+    followers:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    zips:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    }],
+    rezips:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    }],
+    replies:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+    }],
+
+},{timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
